47354242b1aa767b3433f811fe34e67a59b0405a
Porcupinee <htolley0@gmail.com>
2023-12-17 13:59:07 +1000

    Refactor image path in ChessGUI class

--- chess/display.py
+++ chess/display.py
@@ -12,7 +12,7 @@ class ChessGUI:
     def __init__(self, master, board):
         self.master = master
         self.board = board
-        self.image_path = "C:/Users/htoll/Desktop/Projects/Chesster/imgs/"
+        self.image_path = "chess/imgs/"
 
         self.canvas = Canvas(master, width=400, height=400)
         self.canvas.pack()
@@ -63,32 +63,13 @@ class ChessGUI:
                     image = self.piece_images[piece_key]
                     self.canvas.create_image(x0 + 25, y0 + 25, image=image)
 
-import os
 
-def list_files_in_directory(directory):
-    try:
-        files = os.listdir(directory)
-        for file in files:
-            file_path = os.path.join(directory, file)
-            if os.path.isfile(file_path):
-                print(f"File: {file_path}")
-                print(f"Readable: {os.access(file_path, os.R_OK)}")
-    except PermissionError:
-        print(f"Permission denied for directory: {directory}")
-    except FileNotFoundError:
-        print(f"Directory not found: {directory}")
+starting_position = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR"
+loaded_board = LoadPositionFromFen(starting_position)
 
-# Use the function
-list_files_in_directory("C:/Users/htoll/Desktop/Projects/Chesster/imgs/")
+root = tk.Tk()
+root.title("Chess GUI")
 
+chess_gui = ChessGUI(root, loaded_board)
 
-
-# starting_position = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR"
-# loaded_board = LoadPositionFromFen(starting_position)
-
-# root = tk.Tk()
-# root.title("Chess GUI")
-
-# chess_gui = ChessGUI(root, loaded_board)
-
-# root.mainloop()
+root.mainloop()
