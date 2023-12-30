4d86ac136a7ed7201b3500f367dbe830d63d802a
Porcupinee <htolley0@gmail.com>
2023-12-30 16:55:15 +0900

    Add Game class to hold game state

--- exceptions/InvalidMove.py
+++
@@ -1,7 +0,0 @@
-"""
-File for the InvalidMove exception. This exception is raised when a move is invalid.
-""" 
-
-class InvalidMove(Exception):
-    def __init__(self, message) -> None:
-        self.message = message
