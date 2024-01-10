0a444beadba88447997d8b1f19299c5dbb1ef44b
Porcupinee <htolley0@gmail.com>
2024-01-10 22:47:13 +0900

    Add __str__ method to Move class

--- chess/move.py
+++ chess/move.py
@@ -1,10 +1,13 @@
 """move.py:
 This file contains the Move class, which is used to represent a move in the game.
 """
-from pieces import Piece
+from pieces import *
 
 class Move:
     def __init__(self, piece: Piece, current_square: tuple, destination_square: tuple):
         self.piece = piece
         self.current_square = current_square
         self.destination_square = destination_square
+
+    def __str__(self):
+        return f"{convert_piece_to_string(self.piece)} from {self.current_square} to {self.destination_square}"
