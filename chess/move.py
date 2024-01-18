883d5646e2abd0f98b60a729d077d18015b1fa4d
Porcupinee <htolley0@gmail.com>
2024-01-18 12:40:22 -0800

    Add additional attributes and methods to the Move class

--- chess/move.py
+++ chess/move.py
@@ -5,10 +5,14 @@ from pieces import *
 from coordinates import Coordinates
 
 class Move:
-    def __init__(self, piece: Piece, current_coords: Coordinates, destination_coords: Coordinates):
+    def __init__(self, piece: Piece, current_coords: Coordinates, destination_coords: Coordinates, is_en_passant=False, is_castling=False, is_promotion=False, promotion_piece=Piece.NONE):
         self.piece = piece
         self.current_coords = current_coords
         self.destination_coords = destination_coords
+        self.get_is_en_passant = is_en_passant
+        self.get_is_castling = is_castling
+        self.get_is_promotion = is_promotion
+        self.promotion_piece = promotion_piece
 
     def __str__(self):
         return f"{convert_piece_to_string(self.piece)} from {self.current_coords} to {self.destination_coords}"
@@ -30,3 +34,15 @@ class Move:
     
     def __eq__(self, other) -> bool:
         return self.piece == other.piece and self.current_coords == other.current_coords and self.destination_coords == other.destination_coords
+    
+    def get_is_en_passant(self):
+        return self.is_en_passant
+    
+    def get_is_castling(self):
+        return self.is_castling
+    
+    def get_is_promotion(self):
+        return self.is_promotion
+    
+    def get_promotion_piece(self):
+        return self.promotion_piece
