5b7ed70c70a1ab29385675b87e5918d5c5efecd1
Porcupinee <htolley0@gmail.com>
2024-01-18 20:57:19 +0900

    Updated naming to be more precise (current_square -> current_coords)

--- chess/move.py
+++ chess/move.py
@@ -5,19 +5,19 @@ from pieces import *
 from coordinates import Coordinates
 
 class Move:
-    def __init__(self, piece: Piece, current_square: Coordinates, destination_square: Coordinates):
+    def __init__(self, piece: Piece, current_coords: Coordinates, destination_coords: Coordinates):
         self.piece = piece
-        self.current_square = current_square
-        self.destination_square = destination_square
+        self.current_coords = current_coords
+        self.destination_coords = destination_coords
 
     def __str__(self):
-        return f"{convert_piece_to_string(self.piece)} from {self.current_square} to {self.destination_square}"
+        return f"{convert_piece_to_string(self.piece)} from {self.current_coords} to {self.destination_coords}"
     
-    def get_current_square(self):
-        return self.current_square
+    def get_current_coordinates(self):
+        return self.current_coords
     
-    def get_destination_square(self):
-        return self.destination_square
+    def get_destination_coordinates(self):
+        return self.destination_coords
 
     def get_piece(self):
         return self.piece
@@ -29,4 +29,4 @@ class Move:
         return Piece.get_color(self.piece)
     
     def __eq__(self, other) -> bool:
-        return self.piece == other.piece and self.current_square == other.current_square and self.destination_square == other.destination_square
+        return self.piece == other.piece and self.current_coords == other.current_coords and self.destination_coords == other.destination_coords
