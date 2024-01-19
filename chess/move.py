c9783bce4ca6f188461b721450feee66d9662d72
Porcupinee <htolley0@gmail.com>
2024-01-19 11:51:52 -0800

    Incorporating en_passant, is_castling, is_promotion, promotion_piece to Move class.

--- chess/move.py
+++ chess/move.py
@@ -5,14 +5,19 @@ from pieces import *
 from coordinates import Coordinates
 
 class Move:
-    def __init__(self, piece: Piece, current_coords: Coordinates, destination_coords: Coordinates, is_en_passant=False, is_castling=False, is_promotion=False, promotion_piece=Piece.NONE):
+    def __init__(self, piece: Piece, current_coords: Coordinates, destination_coords: Coordinates, can_en_passant=False, is_castling=False, is_promotion=False, promotion_piece=Piece.NONE):
         self.piece = piece
         self.current_coords = current_coords
         self.destination_coords = destination_coords
-        self.get_is_en_passant = is_en_passant
+        self.can_en_passant = can_en_passant
         self.get_is_castling = is_castling
         self.get_is_promotion = is_promotion
         self.promotion_piece = promotion_piece
+
+        self.en_passant_idx = None
+        if self.get_can_en_passant():
+            print("over here")
+            self.en_passant_idx = Coordinates(self.destination_coords.get_file(), self.current_coords.get_rank() + 1 if Piece.get_color(piece) == Piece.WHITE else self.current_coords.get_rank() - 1).get_board_index()
 
     def __str__(self):
         return f"{convert_piece_to_string(self.piece)} from {self.current_coords} to {self.destination_coords}"
@@ -35,8 +40,8 @@ class Move:
     def __eq__(self, other) -> bool:
         return self.piece == other.piece and self.current_coords == other.current_coords and self.destination_coords == other.destination_coords
     
-    def get_is_en_passant(self):
-        return self.is_en_passant
+    def get_can_en_passant(self):
+        return self.can_en_passant
     
     def get_is_castling(self):
         return self.is_castling
@@ -46,3 +51,6 @@ class Move:
     
     def get_promotion_piece(self):
         return self.promotion_piece
+    
+    def get_en_passant_idx(self):
+        return self.en_passant_idx
